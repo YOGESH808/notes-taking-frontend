7ce3c3980ca7ffb8ae456ae530b57a3a00514e2b
Yogesh Agarwal <krishnaag808@gmail.com>
2024-01-05 04:56:02 +0530

    login component created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Login from "./components/Auth/Login";
 import Signup from "./components/Auth/Signup";
 
 function App() {
   return (
-    <Signup/>
+    <Login/>
   );
 }
 
--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,41 @@
-import React from 'react'
-
+import React,{useState} from 'react';
+import axios from 'axios';
 export default function Login() {
-  return (
-    <div>Login</div>
-  )
+	const [formData, setFormData] = useState({
+		usernameOrEmail: '',
+		password: '',
+	});
+
+	const handleChange = (e) => {
+		// console.log({...formData},e.target.name);
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const requestData = {
+				"username":formData.usernameOrEmail,
+				"password":formData.password
+			}
+			const response = await axios.post('http://localhost:4000/api/auth/login',requestData);
+			console.log(response.data); // Handle success
+		} catch (error) {
+			console.error(error); // Handle error
+		}
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<label>
+				Username or Email:
+				<input type="text" name="usernameOrEmail" value={formData.usernameOrEmail} onChange={handleChange} />
+			</label>
+			<label>
+				Password:
+				<input type="password" name="password" value={formData.password} onChange={handleChange} />
+			</label>
+			<button type="submit">Login</button>
+		</form>
+	);
 }
