a4240f151dcf080e8dfd097b40d9c0817c9ab436
Yogesh Agarwal <krishnaag808@gmail.com>
2024-01-14 01:01:36 +0530

    note details page

---
+++ src/components/Notes/NoteDetails.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import React, { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+function NoteDetails() {
+  const { noteId } = useParams();
+    const [note,setNote] = useState([]);
+  const fetchDetails = useCallback(async () => {
+    const noteResponse = await axios.get(`http://localhost:4000/api/notes/${noteId}`, {
+      headers: {
+        Authorization: `Bearer${localStorage.getItem("token")}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    setNote(noteResponse.data);
+  });
+  useEffect(() => {
+    //fetch the details of the note
+    fetchDetails();
+  }, []);
+  return (
+    <>
+      <div className="note-container">
+      <h1>Title: {note.title}</h1>
+      <p>Content: {note.content}</p>
+      <p>Owner: {note.owner}</p>
+      <p>Created At: {note.createdAt}</p>
+      <p>Updated At: {note.updatedAt}</p>
+    </div>
+    </>
+  );
+}
+
+export default NoteDetails;
